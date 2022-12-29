7598edafc649e5e28c1851f1567e3e2782540eb5
shwetamendhegiri <95839937+shwetamendhegiri@users.noreply.github.com>
2022-12-29 13:29:28 +0530

    Add files via upload

---
+++ Question4.java
@@ -0,0 +1,25 @@
+package assignment1;
+
+public class Question4 {
+
+	public static void main(String[] args) {
+		
+		int n=14;
+		for(int i=0;i<n;i++)
+		{	
+			for(int j=0;j<n;j++)
+			{
+				if(i==n-1||j==0&&i>=n/2||j==n-1&&i>=n/2||i-j>=(n-1)/2||i+j>=n+(n-1)/2-1)
+				{
+				    System.out.print("*");
+				}
+				else
+				{
+					System.out.print(" ");
+				}
+			}
+			System.out.println();
+			}
+		}
+	}
+
